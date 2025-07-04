bde306e5a7b2f09f86dc431265e78f793c219285
Domenique412 <dwatkinswise@gmail.com>
2025-07-04 15:41:38 -0400

    made all corrections

--- scripts/index.js
+++ scripts/index.js
@@ -17,32 +17,39 @@ const profileNameEl = document.querySelector(".profile__name")
 const profileDescriptionEl = document.querySelector(".profile__description")
 
 
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
 
 
 editProfileBtn.addEventListener("click", function () {
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-  editProfileModal.classList.add("modal_is-opened")
+ openModal(editProfileModal);
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened")
+  closeModal(editProfileModal)
 });
 
 
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened")
+  openModal(newPostModal)
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened")
+  closeModal(newPostModal)
 });
 
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
     profileNameEl.textContent = editProfileNameInput.value;
     profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-editProfileModal.classList.remove("modal_is-opened")
+closeModal(editProfileModal)
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
@@ -55,8 +62,8 @@ const imageLink = newPostImageLinkInput.value;
   const caption = newPostCaptionInput.value;
   console.log("Image Link:", imageLink);
   console.log("Caption:", caption);
-   newPostImageLinkInput.value = "";
-  newPostCaptionInput.value = "";
+  newPostForm.reset();
+  closeModal(newPostModal)
 }
 
 newPostForm.addEventListener('submit', handleNewPostSubmit);
